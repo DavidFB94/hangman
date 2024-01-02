1d54ebe6470542e50d0574d9cc64fd989d4f9608
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-01-02 12:57:40 +0000

    Add hangman code for input warnings

--- hangman.py
+++ hangman.py
@@ -37,3 +37,9 @@ def hangman():
             else:
                 lives = lives - 1 #takes away a life if wrong
                 print('Letter is not in word.')
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character(s). Please try again.')
