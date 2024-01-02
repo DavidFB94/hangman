c34207f3bbe72f6e366c7425501aa552de558357
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-01-02 12:55:51 +0000

    Add hangman code to display word and lives

--- hangman.py
+++ hangman.py
@@ -16,3 +16,12 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('\nYou have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
